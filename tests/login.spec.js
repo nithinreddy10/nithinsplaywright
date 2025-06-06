c6489de683a151879c69dae6a08e638875c08e20
Nithin Reddy <nputta@petsmart.com>
2025-06-05 22:32:29 -0500

    removed some files

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,12 +1,14 @@
 import { test, expect } from '@playwright/test';
 
+import data from "../testData/login.json"
+
 test ("verify login with valid credential", async ({page})=>{
 
 await page.goto("https://development.pilot.petsmart.com/?experimentGroup=fullsolution")
 
 await page.locator("//a[@class='sparky-c-text-link legacy-signin__link']//span[1]").click()
 
-await page.locator("(//input[@name='email'])[1]").fill("nithinreddymao+vipp@gmail.com")
+await page.locator("(//input[@name='email'])[1]").fill("data.")
 
 await page.locator("(//input[@name='password'])[1]").fill("ABHIxyz1@")
 
--- tests/saucedemo.spec.js
+++ tests/saucedemo.spec.js
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-//import data from "../testData/addemployee.json"
+import data from "../testData/UserName.json"
 
 test("verify login with valid credential in saucedemo webpage", async ({ page }) => {
 
     await page.goto("https://www.saucedemo.com/v1/")
 
-    await page.locator("(//input[@class='form_input'])[1]").fill(saucelab)
+    await page.locator("(//input[@class='form_input'])[1]").fill(data.firstname)
 
-    await page.locator("(//input[@class='form_input'])[2]").fill(nbb)
+    await page.locator("(//input[@class='form_input'])[2]").fill(data.lastname)
 
     await page.locator("//input[@type='submit']").click()
 
